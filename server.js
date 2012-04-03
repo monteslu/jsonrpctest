0b0bfa886633b8a3d9979ab77034a93183ce4bfe
Luis Montes <monteslu@gmail.com>
2012-04-03 12:29:58 -0700

    port

--- server.js
+++ server.js
@@ -129,7 +129,7 @@ app.get('/smd',function(req,resp){
 
 
 
-app.listen(16562);
+app.listen(16564);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 
